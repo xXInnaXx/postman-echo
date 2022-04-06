a8f5f6b91a869d0312418fe03b4c72d0fdb82c75
innakhabirova <iikhabi@mail.ru>
2022-04-06 18:19:27 +0300

    Postman-echo test was created and tested

---
+++ src/test/java/ru/netology/postmanecho/TestPostman.java
@@ -0,0 +1,27 @@
+package ru.netology.postmanecho;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TestPostman {
+
+    @Test
+    void shouldReturn() {
+        String some_data = "some data";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(some_data) // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+
+}
