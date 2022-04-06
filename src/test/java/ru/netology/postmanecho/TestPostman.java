ab715727c77410cfc14989ebf88f152adf4d01dc
innakhabirova <iikhabi@mail.ru>
2022-04-06 18:29:43 +0300

    TestPostman was tested

--- src/test/java/ru/netology/postmanecho/TestPostman.java
+++ src/test/java/ru/netology/postmanecho/TestPostman.java
@@ -19,7 +19,7 @@ public class TestPostman {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some value"))
         ;
     }
 
