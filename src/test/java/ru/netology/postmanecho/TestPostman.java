9445476ce3f9998c7a4b4c712ed784a083c5715a
innakhabirova <iikhabi@mail.ru>
2022-04-06 18:33:34 +0300

    TestPostman changed

--- src/test/java/ru/netology/postmanecho/TestPostman.java
+++ src/test/java/ru/netology/postmanecho/TestPostman.java
@@ -19,7 +19,7 @@ public class TestPostman {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 
